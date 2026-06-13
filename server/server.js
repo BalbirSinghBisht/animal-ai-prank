a1d19cef013fbf757205202a2c8c4f0b57b77293
Chetan Singh Bisht <chetanbisht1311@gmail.com>
2026-06-13 12:23:12 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -57,4 +57,22 @@ app.post('/api/answer', async (req, res) => {
     success: true
   });
 });
+
+const path = require('path');
+
+app.use(
+  express.static(
+    path.join(__dirname, '../client/dist')
+  )
+);
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.join(
+      __dirname,
+      '../client/dist/index.html'
+    )
+  );
+});
+
 app.listen(process.env.PORT || 5000,()=>console.log('running'));
